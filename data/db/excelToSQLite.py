d19316a70b3daf012bd7ac084d0710d971121a98
Aron Baldovino <201708851@uni.au.dk>
2020-03-23 15:51:36 +0100

    comments in code translated to english

--- data/db/excelToSQLite.py
+++ data/db/excelToSQLite.py
@@ -1,6 +1,6 @@
 import sqlite3
 import pandas as pd
-filename = "meanGradesAll"
+filename = "meanGradesAll" #Name of file for conversion
 con=sqlite3.connect(filename+".db")
-wb=pd.read_excel(filename+'.xlsx')
-wb.to_sql('Mean', con)
+wb=pd.read_excel(filename+'.xlsx') #read excel file
+wb.to_sql('Mean', con) #Convert
