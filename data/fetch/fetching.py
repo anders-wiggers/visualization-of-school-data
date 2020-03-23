abca1f8aadceca608dd6b90f0444efda522aab76
Anders Wiggers <info@anderswiggers.dk>
2020-03-23 10:53:10 +0100

    new gitignore files

---
+++ data/fetch/fetching.py
@@ -0,0 +1,13 @@
+import config
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+
+print(config.HEAD)
+
+#options = Options()
+# options.set_headless(headless=config.HEAD)
+#browser = webdriver.Chrome(chrome_options = options, executable_path = os.getcwd()+'/assets/chromedriver')
