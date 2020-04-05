7e2ea971c1be124a23da70e61ea36d9322dc0aff
Anders Wiggers <info@anderswiggers.dk>
2020-04-05 11:52:03 +0200

    meangrade extrator

--- data/extractors/meangrade.py
+++ data/extractors/meangrade.py
@@ -29,6 +29,3 @@ con = sqlite3.connect(baseDataDir + "/db/" + filename+".db")
 c = con.cursor()  # The database will be saved in the location where your 'py' file is saved
 
 pd.read_excel(baseDataDir + "/excel/" + excelFilename, index_col=0)
-
-
-print()
