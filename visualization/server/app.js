c66a442b358ae666cecf23f4bdc95805dbc4b1c4
Anders Wiggers <info@anderswiggers.dk>
2020-04-28 10:21:56 +0200

    fixed index

--- visualization/server/app.js
+++ visualization/server/app.js
@@ -1,14 +1,9 @@
-var express = require('express')
-var app = express()
-var path = require('path')
+var express = require('express');
+var app = express();
+var path = require('path');
 
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/main.html'))
-})
-
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static('public'));
 
 app.listen(3000);
+
 console.log('Running at porto 3000');
