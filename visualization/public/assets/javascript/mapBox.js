01632e5eea92042cc8e70b685700b2fff02a3957
Aron Baldovino <201708851@uni.au.dk>
2020-05-13 15:25:12 +0200

    Added multiple selection of communes and zoom adjusted to selection

--- visualization/public/assets/javascript/mapBox.js
+++ visualization/public/assets/javascript/mapBox.js
@@ -2,13 +2,17 @@
 var map = new L.Map('map');
 var osmUrl = 'https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=523123eab9074addb51cc220ddc9df2d';
 var osm = new L.TileLayer(osmUrl, { minZoom: 5, maxZoom: 18 });
-
+//Adding map, plus zoom of denmark
 map.addLayer(osm);
 map.setView(new L.LatLng(56.283, 10.491), 6.58);
-
+// Initializing mini map
 var osm2 = new L.TileLayer(osmUrl, { minZoom: 0, maxZoom: 13 });
 var miniMap = new L.Control.MiniMap(osm2, { toggleDisplay: true }).addTo(map);
 
+let selectedCommunes = [];
+let selectedCommunesNames = [];
+let boundArray = []
+let selectMultiple = false;
 var mapJSON;
 var inBounds = [];
 var myLayer = new L.LayerGroup().addTo(map);
@@ -32,7 +36,7 @@ fetch('/assets/geojson/kommuner.geojson')
 				color: 'white',
 				dashArray: '',
 				fillOpacity: 0.01,
-				onEachFeature: onEachFeature
+				onEachFeature: onEachFeature,
 			}).addTo(map);
 		});
 	})
@@ -42,7 +46,6 @@ fetch('/assets/geojson/kommuner.geojson')
 
 function highlightFeature(e) {
 	var layer = e.target;
-
 	layer.setStyle({
 		weight: 1,
 		color: 'red'
@@ -54,21 +57,118 @@ function highlightFeature(e) {
 	info.update(layer.feature.properties);
 }
 
+
+
 function resetHighlight(e) {
-	geojson.resetStyle(e.target);
-	info.update();
+	if(selectedCommunesNames.includes(e.target.feature.properties.KOMNAVN) === true){
+
+	} else {
+		geojson.resetStyle(e.target);
+
+		info.update();
+	}
 }
+
 function zoomToFeature(e) {
-	selectedCommunes.push(e.target.feature.properties.KOMNAVN);
+
+	//fetchMarkersAndPlaceOnMap(e);
+}
+
+
+function addMarkerToList(list) {
+	clearList();
+	ul = document.createElement('ul');
+	document.getElementById('mapLi').appendChild(ul);
+	list.forEach(function(item) {
+		let li = document.createElement('li');
+		ul.appendChild(li);
+		li.innerHTML += item;
+	});
+}
+function addKommunesToList(list) {
+	clearKommunes();
+	ul = document.createElement('ul');
+	document.getElementById('kommunes').appendChild(ul);
+	list.forEach(function(item) {
+		let li = document.createElement('li');
+		ul.appendChild(li);
+		li.innerHTML += item;
+	});
+}
+function clearList() {
+	
+	$(document.getElementById('mapLi')).empty();
+}
+function clearKommunes() {
+
+	$(document.getElementById('kommunes')).empty();
+}
+
+function onEachFeature(feature, layer) {
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight,
+		click: determineWhatHappensOnClick,
+	});
+}
+
+function determineWhatHappensOnClick(e){
+ if(selectedCommunesNames.includes(e.target.feature.properties.KOMNAVN) === false) {
+	 selectedCommunes.push(e.target);
+	 selectedCommunesNames.push(e.target.feature.properties.KOMNAVN)
+		selectMultiple = true;
+			addKommunesToList(selectedCommunesNames)
+			e.target.setStyle({
+				weight: 1,
+				color: 'red'
+			})		
+		console.log("im clicking")	
+} else {
+	selectedCommunesNames.splice(selectedCommunesNames.indexOf(e.target.feature.properties.KOMNAVN),1); 
+	addKommunesToList(selectedCommunesNames)
+	e.target.setStyle({
+		weight: 0.01,
+		color: 'white'
+	})
+	markers.clearLayers();
+}
+
+}
+
+var info = L.control();
+
+info.onAdd = function(map) {
+	this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+	this.update();
+	return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function(props) {
+	this._div.innerHTML =
+		'<h4>Vælg kommune</h4>' +
+		(props ? '<b>' + props.KOMNAVN + '</b><br />' + ' Kommune ID: ' + props.KOMKODE : 'Tryk på en kommune brah');
+};
+
+info.addTo(map);
+document.getElementById("markerButton").onclick = function(e) {fetchMarkersAndPlaceOnMap()}
+
+function fetchMarkersAndPlaceOnMap() {
 	markers.clearLayers();
 	contained = [];
-	map.fitBounds(e.target.getBounds());
-	fetch('/api/combine?commune=' + e.target.feature.properties.KOMNAVN + '&year=2019&data=information').then(function(
+	for(let i = 0; i < selectedCommunes.length; i++){
+	boundArray.push(selectedCommunes[i])
+	var group = new L.featureGroup(boundArray)
+	map.fitBounds(group.getBounds());
+	}
+	console.log(selectedCommunes)
+	let i;
+	for(i = 0; i < selectedCommunesNames.length; i++){
+	fetch('/api/combine?commune=' + selectedCommunesNames[i] + '&year=2019&data=information').then(function(
 		response
 	) {
 		response.json().then(function(data) {
 			fullInfoData = data;
-			console.log(data);
 			var i;
 			for (i = 0; i < data.length; i++) {
 				try {
@@ -93,60 +193,20 @@ function zoomToFeature(e) {
 					markers.addLayer(marker);
 					this.map.addLayer(markers);
 				} catch (err) {
-					console.log(err);
+					console.log(err ,"some err")
 				}
 			}
+			
 			map.on('moveend', function() {
 				inBounds = [];
 				var bounds = map.getBounds();
 				myLayer.eachLayer(function(marker) {
 					if (bounds.contains(marker.getLatLng())) {
-						inBounds.push(marker.options.title);
+						inBounds.push(marker.options.kommune);
 					}
 				});
-				addMarkerToList(inBounds);
 			});
 		});
 	});
+	}
 }
-
-function addMarkerToList(list) {
-	clearList();
-	ul = document.createElement('ul');
-	document.getElementById('mapLi').appendChild(ul);
-	list.forEach(function(item) {
-		let li = document.createElement('li');
-		console.log(list.length);
-		ul.appendChild(li);
-		li.innerHTML += item;
-	});
-}
-
-function clearList() {
-	$(document.getElementById('mapLi')).empty();
-}
-
-function onEachFeature(feature, layer) {
-	layer.on({
-		mouseover: highlightFeature,
-		mouseout: resetHighlight,
-		click: zoomToFeature
-	});
-}
-
-var info = L.control();
-
-info.onAdd = function(map) {
-	this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-	this.update();
-	return this._div;
-};
-
-// method that we will use to update the control based on feature properties passed
-info.update = function(props) {
-	this._div.innerHTML =
-		'<h4>Vælg kommune</h4>' +
-		(props ? '<b>' + props.KOMNAVN + '</b><br />' + ' Kommune ID: ' + props.KOMKODE : 'Tryk på en kommune brah');
-};
-
-info.addTo(map);
