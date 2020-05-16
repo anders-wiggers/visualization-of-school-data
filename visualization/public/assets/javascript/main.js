3416cc3272fb39d4b90b30b02b0a7c9fa581ae3d
Anders Wiggers <info@anderswiggers.dk>
2020-05-16 12:37:07 +0200

    first attemt at filtering

--- visualization/public/assets/javascript/main.js
+++ visualization/public/assets/javascript/main.js
@@ -19,6 +19,9 @@ filterButton.addEventListener('click', () => {
 	}
 });
 
+/*
+* MEAN GRADE BUTTON
+*/
 let meanGradeBtn = document.getElementById('grades');
 
 meanGradeBtn.addEventListener('click', () => {
@@ -41,13 +44,14 @@ meanGradeBtn.addEventListener('click', () => {
 	} else {
 		meanGradeBtn.setAttribute('class', 'deactiveFilter');
 		document.getElementById('slider-grades').setAttribute('class', 'hidden');
-		for (let s of fullInfoData) {
-			addMarker(s);
-		}
+
 		gradeStatus = !gradeStatus;
 	}
 });
 
+/*
+* STUDENT NUMBERS BUTTON
+*/
 let studentBtn = document.getElementById('students');
 
 studentBtn.addEventListener('click', () => {
@@ -56,17 +60,16 @@ studentBtn.addEventListener('click', () => {
 
 		document.getElementById('slider-students').setAttribute('class', 'delayedShow');
 
+		studentStatus = !studentStatus;
+
 		fetch(
 			`api/combine?commune=${selectedCommunesNames.join('_')}&year=2019&data=information:students`
 		).then((res) => {
 			res.json().then((data) => {
-				console.log(data);
 				studentsAverage = data;
 				filterData();
 			});
 		});
-
-		studentStatus = !studentStatus;
 	} else {
 		studentBtn.setAttribute('class', 'deactiveFilter');
 		document.getElementById('slider-students').setAttribute('class', 'hidden');
@@ -77,32 +80,55 @@ studentBtn.addEventListener('click', () => {
 
 function filterData() {
 	let temp = [ ...fullInfoData ];
-	let schoolMissingData = [];
 	markers.clearLayers();
 
 	if (gradeStatus) {
 		let min = filteringParameters.grades[0];
 		let max = filteringParameters.grades[1];
 		for (let s of avarageGrade) {
+			let index = temp.findIndex((i) => i.NAME === s.NAME);
+
 			if (s.mean < min || s.mean > max) {
 				//remove all school that doesnt fit from temp
-				let index = temp.findIndex((i) => i.NAME === s.NAME);
-
 				if (index > 0) {
 					temp.splice(index, 1);
 				}
 			} else {
+				if (index > 0) {
+					temp[index].display = true;
+				} else {
+					temp[index].display = false;
+				}
 			}
 		}
 	}
 	if (studentStatus) {
-		console.log('filter students');
+		let min = filteringParameters.students[0];
+		let max = filteringParameters.students[1];
+		for (let s of studentsAverage) {
+			let index = temp.findIndex((i) => i.NAME === s.NAME);
+
+			if (s.classratio < min || s.classratio > max) {
+				//remove all school that doesnt fit from temp
+				if (index > 0) {
+					temp.splice(index, 1);
+				}
+			} else {
+				if (index > 0) {
+					temp[index].display = true;
+				} else {
+					temp[index].display = false;
+				}
+			}
+		}
 	}
 
-	console.log('temp after: ', temp);
+	updateMakers(temp);
+}
 
-	for (let s of temp) {
-		addMarker(s);
+function updateMakers(arr) {
+	for (let s of arr) {
+		if (s.display) addMarker(s);
 	}
 }
 
